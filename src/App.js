64a60f80fd716cdfd2f7d69950e6420129f2d551
Iesus <jguerrasal890@gmail.com>
2022-08-17 22:10:21 -0500

    Removed React.Fragment

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import Nav from './Components/Nav.js';
 function App() {
 
   return(
-    <>
-    <nav id="nav">
-      <img src="/logo.png" alt="kusa-news" width="150px"></img>
-      <Nav text="MÁS RECIENTES | トップ"/>
-      <Nav text="ENTRETENIMIENTO | エンタメ"/>
-      <Nav text="DEPORTES | スポーツ"/>
-      <Nav text="POLÍTICA | 政治"/>
-    </nav>
-    </>)
+    <header>
+      <nav id="nav">
+        <img src="/logo.png" alt="kusa-news" width="150px"></img>
+        <Nav text="MÁS RECIENTES | トップ"/>
+        <Nav text="ENTRETENIMIENTO | エンタメ"/>
+        <Nav text="DEPORTES | スポーツ"/>
+        <Nav text="POLÍTICA | 政治"/>
+      </nav>
+    </header>
+    )
 
 }
 
